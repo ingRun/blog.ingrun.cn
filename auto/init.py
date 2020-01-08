3eca7592b86a1f3d02f2350bb5df56f4538fced3
ingrun <ingrun@163.com>
2020-01-08 15:08:42 +0800

    3

--- auto/init.py
+++ auto/init.py
@@ -10,10 +10,13 @@ while k is not '0':
         os.system('python3 c.py && python3 b.py')
     elif k == '4':
         os.system('python3 b.py')
+    elif k == '5':
+        os.system('python3 c.py')
     print('1:  同步远程仓库， 并重启项目！')
     print('2:  仅同步远程仓库')
     print('3:  重启项目')
     print('4:  运行项目')
+    print('5:  关闭项目')
     print('0:  退出')
     print()
     k = input('请选择：')
