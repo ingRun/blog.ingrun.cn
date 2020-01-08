29c613f97b7bb7865b0c8f06594abf680245aa5e
ingrun <ingrun@163.com>
2020-01-08 15:30:39 +0800

    update c.py

--- auto/c.py
+++ auto/c.py
@@ -1,5 +1,6 @@
 import subprocess
 
+ls: list = []
 cmd = 'ps -ef | grep blog.py'
 
 p = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True)
@@ -7,10 +8,11 @@ p = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True)
 out = str(p.stdout, 'utf-8').split('\n')
 
 for i in range(len(out)-1):
-    link = out[i].split('     ')
-    link = link[1]
-    link = link.split('  ')
-    print(link[0])
+    link = out[i].split(' ')
+    ls = []
+    for k in (link):
+        if k is not ' ' and k is not '':
+            ls.append(k)
 
-    cmd = f'kill -9 {link[0]}'
+    cmd = f'kill -9 {ls[1]}'
     subprocess.run(cmd, stdout=subprocess.PIPE, shell=True)
